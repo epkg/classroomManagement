862de0d97a8cfc774a4f69e59abfae1b41e653db
Hiroshi Shiratsuchi <sira@fuk.kindai.ac.jp>
2026-08-06 17:04:16 +0900

    token.pickle に代えて token.json 形式で保存するように変更

--- classroomManagement.py
+++ classroomManagement.py
@@ -1,5 +1,4 @@
 # coding: UTF-8
-import pickle
 import os.path
 import sys
 import csv
@@ -14,6 +13,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 #
 from docopt import docopt
 from tqdm import tqdm
@@ -137,17 +137,13 @@ def api_init():
               "https://www.googleapis.com/auth/classroom.rosters",
               "https://www.googleapis.com/auth/classroom.profile.emails",
               "https://www.googleapis.com/auth/classroom.announcements.readonly"]
-    # If modifying these scopes, delete the file token.pickle.
-    # TODO: token.pickle は pickle ベースの旧形式。google-auth の新バージョンでは
-    #       token.json (creds.to_json() / Credentials.from_authorized_user_info()) が推奨。
-    #       Python バージョンアップ時にライブラリも更新する場合は合わせて移行すること。
+    # If modifying these scopes, delete the file token.json.
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
+    # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", scopes)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -157,8 +153,8 @@ def api_init():
                 "credentials.json", scopes)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
     _service_classroom = build("classroom", "v1", credentials=creds)
     return creds, _service_classroom
 
